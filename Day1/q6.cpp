f3fdca72145a736b5cdaaf5a01759ccfa84b54ee
Bhavya Gupta <77940309+Bhavya-gupta127@users.noreply.github.com>
2023-06-07 01:45:40 +0530

    Create q6.cpp
    Day 1 done

---
+++ Day1/q6.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn=INT_MAX,ans=0;
+        for(auto &i:prices)
+        {
+            mn=min(mn,i);
+            ans=max(ans,i-mn);
+        }
+        return ans;
+    }
+};
